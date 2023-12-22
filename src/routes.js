092843471be09cd32cdc9a0aa2e6066cd3de898c
Nipu Chakraborty <nipuchakraborty86@hotmail.com>
2023-12-23 02:14:49 +0600

    swagger documentation added in route

---
+++ src/routes.js
@@ -0,0 +1,89 @@
+
+const {uploadFile, downloadFile, deleteFile} = require('./filesSharing/files.controller')
+const {generateKeys} = require('./filesSharing/files.service')
+const {fileValidationSchema} = require("./filesSharing/validation/file.validation")
+const Router = require('express').Router
+const router = Router();
+// Routes
+/**
+ * @swagger
+ * /:
+ *   get:
+ *     summary: Health check endpoint
+ *     description: Returns a 200 OK response.
+ *     responses:
+ *       200:
+ *         description: OK
+ */
+router.get("/", (req, res) => res.sendStatus(200));
+/**
+ * @swagger
+ * /files:
+ *   post:
+ *     summary: Upload a file
+ *     description: Upload a file with validation and key generation.
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         multipart/form-data:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               file:
+ *                 type: string
+ *                 format: binary
+ *     responses:
+ *       200:
+ *         description: File uploaded successfully
+ *       400:
+ *         description: Bad request
+ *       500:
+ *         description: Internal server error
+ */
+router.post('/files',fileValidationSchema, generateKeys , uploadFile);
+/**
+ * @swagger
+ * /files/{publicKey}:
+ *   get:
+ *     summary: Download a file
+ *     description: Download a file using the public key.
+ *     parameters:
+ *       - in: path
+ *         name: publicKey
+ *         required: true
+ *         schema:
+ *           type: string
+ *     responses:
+ *       200:
+ *         description: File downloaded successfully
+ *       404:
+ *         description: File not found
+ *       500:
+ *         description: Internal server error
+ */
+router.get('/files/:publicKey', downloadFile);
+
+
+/**
+ * @swagger
+ * /files/{privateKey}:
+ *   delete:
+ *     summary: Delete a file
+ *     description: Delete a file using the private key.
+ *     parameters:
+ *       - in: path
+ *         name: privateKey
+ *         required: true
+ *         schema:
+ *           type: string
+ *     responses:
+ *       200:
+ *         description: File deleted successfully
+ *       404:
+ *         description: File not found
+ *       500:
+ *         description: Internal server error
+ */
+router.delete('/files/:privateKey', deleteFile);
+
+exports.router = router
