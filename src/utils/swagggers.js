0d262adcbdfe02a8f6128e48efdfb8c01da78676
Nipu Chakraborty <nipuchakraborty86@hotmail.com>
2023-12-23 02:18:15 +0600

    dummy files

---
+++ src/utils/swagggers.js
@@ -0,0 +1,33 @@
+const swaggerJSDoc = require("swagger-jsdoc");
+const {version} = require("../../package.json")
+const swaggerUi = require("swagger-ui-express");
+const log = require("./logger");
+
+const options ={
+    definition: {
+        openapi: "3.0.0",
+        info: {
+          title: "REST API Docs",
+          version,
+        }
+      },
+      apis: ["./src/routes.ts"],
+    }; 
+
+
+    const swaggerSpec = swaggerJSDoc(options);
+
+    function swaggerDocs(app, port) {
+      // Swagger page
+      app.use("/docs", swaggerUi.serve, swaggerUi.setup(swaggerSpec));
+    
+      // Docs in JSON format
+      app.get("/docs.json", (req, res) => {
+        res.setHeader("Content-Type", "application/json");
+        res.send(swaggerSpec);
+      });
+    
+      console.log(`Docs available at http://localhost:${port}/docs`);
+    }
+    
+module.exports=swaggerDocs;
